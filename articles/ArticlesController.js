11e577fa933113e4c4662add4f646c37d0e61d70
Victorpitangui001 <pitanguigit@gmail.com>
2024-01-07 00:03:10 -0300

    finalização de formulario de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const CategorieModel = require("../categories/CategorieModel");
 
 router.get("/articles", (req, res) => {
-    res.send("rota de cartigos")
+    res.send("rota de artigos");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-    res.render("admin/articles/new")
+    CategorieModel.findAll().then(categories => {
+        res.render("admin/articles/new", { categories: categories });
+    })
+    .catch(error => {
+        console.error("Erro ao buscar categorias:", error);
+        res.render("admin/articles/new", { categories: [] }); // Renderiza com um array vazio em caso de erro
+    });
 });
 
-module.exports = router
+module.exports = router;
