3f285867564b589e52b0c22312f4fdcd54402353
Victorpitangui001 <pitanguigit@gmail.com>
2024-01-06 21:36:48 -0300

    Criação do script de edição

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -13,8 +13,8 @@ router.post("/categories/save", (req, res) => {
         CategoriesController.create({
             title: title,
             slug: slugify(title)
-        }).then(() =>{
-            res.redirect("/");
+        }).then(() => {
+            res.redirect("/admin/categories");
         })
     } else {
         res.redirect('/admin/categories/new');
@@ -23,9 +23,56 @@ router.post("/categories/save", (req, res) => {
 
 router.get("/admin/categories", (req, res) => {
     CategoriesController.findAll().then(categories => {
-        res.render("admin/categories/index", {categories:categories});
+        res.render("admin/categories/index", { categories: categories });
     });
-   
-})
+
+});
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            CategoriesController.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        } else {
+            res.redirect("/admin/categories");
+        }
+    } else {
+        res.redirect("/admin/categories");
+    }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+    CategoriesController.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category })
+        } else {
+            res.redirect("/admin/categories")
+        }
+    }).catch(error => {
+        res.redirect("/admin/categories")
+    });
+});
+
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    CategoriesController.update({ title: title, slug: slugify(title) }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    })
+});
 
 module.exports = router
